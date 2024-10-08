b4b8ae5e162d2f5235aaf349945ebd6b29766f80
Jesse Okeya <jesse.okeya@extend.com>
2024-10-08 14:55:34 -0400

    feat: Setup project with auto deploy to aws via sst

--- sst.config.ts
+++ sst.config.ts
@@ -1,12 +1,19 @@
 /// <reference path="./.sst/platform/config.d.ts" />
-
 export default $config({
   app(input) {
     return {
       name: "portfolio",
       removal: input?.stage === "production" ? "retain" : "remove",
+      region: "us-east-1",
       home: "aws",
+      providers: { cloudflare: "5.40.1" },
     };
   },
-  async run() {},
+  async run() {
+    const { web } = await import("./stacks");
+    return {
+      webUrl: web.url,
+      environment: $app.stage,
+    };
+  },
 });
