05f6bb36febd471ab816d0ace539e409ed504d70
Jesse Okeya <jesse.okeya@extend.com>
2024-10-08 17:23:49 -0400

    feat: Setup project with auto deploy to aws

--- stacks/web.ts
+++ stacks/web.ts
@@ -3,7 +3,7 @@ import { NextjsArgs } from "../.sst/platform/src/components/aws";
 const props: NextjsArgs = {
   path: "apps/web",
   environment: {
-    AWS_REGION: process.env.AWS_REGION || "us-east-1",
+    NODE_ENV: $app.stage === "production" ? "production" : "development",
     CLOUDFLARE_API_TOKEN: process.env.CLOUDFLARE_API_TOKEN || "",
     CLOUDFLARE_DEFAULT_ACCOUNT_ID:
       process.env.CLOUDFLARE_DEFAULT_ACCOUNT_ID || "",
