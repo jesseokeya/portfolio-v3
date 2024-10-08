56c4d374d6124b95cba837a405a7bcdbd64078cf
Jesse Okeya <jesse.okeya@extend.com>
2024-10-08 14:58:53 -0400

    feat: Setup project with auto deploy to aws via sst

--- stacks/index.ts
+++ stacks/index.ts
@@ -1,18 +1,1 @@
-<<<<<<< HEAD
 export * from "./web";
-=======
-import { NextjsArgs } from "../.sst/platform/src/components/aws";
-
-const props: NextjsArgs = {};
-
-if ($app.stage === "production") {
-  props.domain = {
-    name: "jesseokeya.com",
-    dns: sst.cloudflare.dns(),
-  };
-}
-
-const web = new sst.aws.Nextjs("Web", props);
-
-export { web };
->>>>>>> main
