ab58010b272e6e9699127dbb261ca3f032af2bb4
Jesse Okeya <jesse.okeya@extend.com>
2024-10-10 04:18:28 -0400

    refactor: Restructure repo by moving Next.js app to root & update blog content (#8)
    * feat: Update blog content & restrucuture repo putting nextjs at the root
    
    * feat: Update blog content & restrucuture repo putting nextjs at the root
    
    * feat: Update blog content & restrucuture repo putting nextjs at the root

--- stacks/web.ts
+++ stacks/web.ts
@@ -3,10 +3,10 @@ import { NextjsArgs } from "../.sst/platform/src/components/aws";
 const props: NextjsArgs = {
   path: "apps/web",
   environment: {
-    NODE_ENV: $app.stage === "production" ? "production" : "development",
     CLOUDFLARE_API_TOKEN: process.env.CLOUDFLARE_API_TOKEN || "",
     CLOUDFLARE_DEFAULT_ACCOUNT_ID:
       process.env.CLOUDFLARE_DEFAULT_ACCOUNT_ID || "",
+    NODE_ENV: $app.stage === "production" ? "production" : "development",
   },
 };
 
