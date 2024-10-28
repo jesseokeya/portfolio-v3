fed095482e26b9a79c255f8a8f0efcc42811a997
Jesse Okeya <Jesseokeya@gmail.com>
2024-10-28 11:48:35 -0400

    feat: Update web.ts

--- stacks/web.ts
+++ stacks/web.ts
@@ -6,7 +6,7 @@ const props: NextjsArgs = {
     CLOUDFLARE_API_TOKEN: process.env.CLOUDFLARE_API_TOKEN || "",
     CLOUDFLARE_DEFAULT_ACCOUNT_ID:
       process.env.CLOUDFLARE_DEFAULT_ACCOUNT_ID || "",
-    NODE_ENV: $app.stage === "production" ? "production" : "development",
+    NODE_ENV: $app.stage,
   },
 };
 
